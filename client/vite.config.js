b9e14da66a028c0d7c2b5f023781d57582fa9928
Nhan_Tran <nhantranminecraft@gmail.com>
2025-03-26 15:22:48 +0700

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Cho phép truy cập từ các thiết bị khác
+    port: 5173, // Cổng mặc định của Vite
+  },
+});
